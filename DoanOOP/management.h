ab541938055d10a17676cfab323d96fc90de3916
PolKettt <ketbestww@gmail.com>
2025-03-24 21:04:52 +0700

    update

--- DoanOOP/management.h
+++ DoanOOP/management.h
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "book.h"
+#include "customer.h"
+using namespace std;
+
+class bookManagement {
+private:
+	vector<book> bookList; // list theo ID nhe
+public:
+	//nhap sach
+	void addBook() {
+	}
+	//xoa sach
+	void removeBook() {
+	}
+	//sua sach
+	void findBook() {
+	}
+	//in thong tin sach
+	void displayBook() {
+	}
+	//in danh sach sach
+	void displayBookList() {
+	}
+	//in ra file text
+	void exportToFile() {
+	}
+};
+
+
 #pragma once
